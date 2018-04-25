8c3639f0b46bb4f5e9d7dbed7a17f37690c82ff1
lipengallan <lipengallan@163.com>
2018-04-25 22:04:29 +0800

    new version with create query

--- mypython/my_sqlalchemy.py
+++ mypython/my_sqlalchemy.py
@@ -1,49 +1,103 @@
 #!/usr/bin/env python
 # coding:utf-8
 
-
-from sqlalchemy import Column, Integer, String, create_engine,exc
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, String, Column, create_engine, exc
 from sqlalchemy.orm import sessionmaker
+from random import randint
+
 
 Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'user';
-    id = Column(Integer,primary_key=True)
-    name = Column(String(10), primary_key=True)
-
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    age = Column(Integer)
 
     def __str__(self):
-        return ''.join([login,userid,projid])
+        return '%s %s %s' %(self.id, self.name, self.age)
 
 
-def test_create_insert():
-    port = 30023
-    engine = create_engine('mysql+mysqldb://root:root@localhost:30023/mytest?unix_socket=/usr/local/mariadb/mysql.sock')
-    try:
-        kwargs = {'unix_socket':'/usr/local/mariadb/mysql.sock'}
-        engine.connect()
-    except exc.OperationalError:
-        engine = create_engine('mysql+mysqldb://root:lipeng#opzoon@localhost:30023/?unix_socket=/usr/local/mariadb/mysql.sock')
-        engine.execute('create database mytest',**kwargs).close()
-        engine = create_engine('mysql+mysqldb://root:lipeng#opzoon@localhost:30023/mytest?unix_socket=/usr/local/mariadb/mysql.sock')
+class Article(Base):
+    __tablename__ = 'article'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    pages = Column(Integer)
+
+    def __str__(self):
+        return '%s %s %s' %(self.id, self.name, self.pages)
 
 
+engine = create_engine('mysql+mysqldb://root:lipeng#opzoon@localhost/mytest?unix_socket'
+                       '=/usr/local/mariadb/mysql.sock')
 
-    Base.metadata.create_all(engine)
-    DBsession = sessionmaker(bind=engine)
-    session = DBsession()
+# create table mytest.users and mytest.article
+Base.metadata.create_all(engine)
+sessionDB = sessionmaker(bind=engine)
 
-    new_user = User(id='5',name='Bob')
-    session.add(new_user)
+
+def add_user(id, name, age):
+    user = User(id=id, name=name, age=age)
+    session = sessionDB()
+    session.add(user)
     session.commit()
     session.close()
 
 
-if __name__ == '__main__':
-    test_create_insert()
+def add_article(id, name, pages):
+    article = Article(id=id, name=name, pages=pages)
+    session = sessionDB()
+    session.add(article)
+    session.commit()
+    session.close()
 
 
+def query_user_byId(id):
+    session = sessionDB()
+    user = session.query(User).filter(User.id == id).first()
+    session.close()
+    print user
 
+
+def query_user_byIdAndAge(id,age):
+    session = sessionDB()
+    for each in session.query(User).filter(User.id > id).filter(User.age > age):
+        print each
+    session.close()
+
+
+def query_user_byIdRange(id):
+    import pdb
+    # pdb.set_trace()
+    session = sessionDB()
+    for each in session.query(User).filter(User.id > id):
+        print each
+    session.close()
+
+
+def query_user_all():
+    session = sessionDB()
+    for each in session.query(User).order_by(User.age):
+        print each
+    session.close()
+
+
+for i in range(0):
+    try:
+        article_id = randint(1, 100)
+        article_pages = randint(30, 500)
+        add_article(article_id, "book_%s"%i, article_pages)
+
+        user_id = randint(1, 100)
+        user_age = randint(90, 200)
+        add_user(user_id, "lipeng_%s" %i, user_age)
+    except exc.IntegrityError as e:
+        if 'Duplicate entry' in e[0]:
+            continue
+
+query_user_byIdRange(30)
+print '---------------------------'
+query_user_byIdAndAge(30,120)
+#query_user_all()
