f2aa5bcc28c99c9fddf8039ff9d10d273baf506a
lipengallan <lipengallan@163.com>
2018-04-26 13:36:53 +0800

    add delete update

--- mypython/my_sqlalchemy.py
+++ mypython/my_sqlalchemy.py
@@ -46,6 +46,12 @@ def add_user(id, name, age):
     session.close()
 
 
+def add_user_all(User_list):
+    session = sessionDB()
+    session.add_all(User_list)
+    session.commit()
+    session.close()
+
 def add_article(id, name, pages):
     article = Article(id=id, name=name, pages=pages)
     session = sessionDB()
@@ -84,20 +90,60 @@ def query_user_all():
     session.close()
 
 
+def modify_name_byid(id):
+    session = sessionDB()
+    user = session.query(User).filter(User.id == id).first()
+    user.name = 'hehehehe'
+    session.commit()
+    session.close()
+
+
+
+def delete_user_byId(id):
+    session = sessionDB()
+    session.query(User).filter(User.id == id).delete()
+    session.commit()
+    session.close()
+
+
+def delete_user_byIdRange(id):
+    session = sessionDB()
+    session.query(User).filter(User.id < id).delete()
+    session.commit()
+    session.close()
+
+
 for i in range(0):
     try:
-        article_id = randint(1, 100)
+        article_id = randint(10, 100)
         article_pages = randint(30, 500)
         add_article(article_id, "book_%s"%i, article_pages)
 
-        user_id = randint(1, 100)
+        user_id = randint(10, 100)
         user_age = randint(90, 200)
         add_user(user_id, "lipeng_%s" %i, user_age)
     except exc.IntegrityError as e:
         if 'Duplicate entry' in e[0]:
             continue
 
+user_one = User(id=9,name='9',age=9)
+user_two = User(id=8,name='8',age='8')
+
+
+#add_user_all([user_one,user_two])
+
+
+
+
 query_user_byIdRange(30)
 print '---------------------------'
 query_user_byIdAndAge(30,120)
-#query_user_all()
+
+
+#modify_name_byid(99)
+#query_user_byId(99)
+
+#delete_user_byId(99)
+
+
+delete_user_byIdRange(1000)
