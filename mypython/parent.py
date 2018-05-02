0e989cac188d2ff75449be6eeabfe7677228e527
lipengallan <lipengallan@163.com>
2018-05-02 08:49:44 +0800

    querying with joins

--- mypython/parent.py
+++ mypython/parent.py
@@ -9,11 +9,13 @@ Base = declarative_base()
 class Parent(Base):
     __tablename__ = 'parent'
     id = Column(Integer, primary_key=True)
+    name = Column(String(20))
     children = relationship("Child", back_populates="parent")
 
 class Child(Base):
     __tablename__ = 'child'
     id = Column(Integer, primary_key=True)
+    name = Column(String(20))
     parent_id = Column(Integer, ForeignKey('parent.id'))
     parent = relationship("Parent", back_populates="children")
 
@@ -23,3 +25,33 @@ engine = create_engine('mysql+mysqldb://root:lipeng#opzoon@localhost/relation?un
                        '=/usr/local/mariadb/mysql.sock')
 Base.metadata.create_all(engine) 
 
+
+sessionDB = sessionmaker(bind=engine)
+session = sessionDB()
+
+'''
+# create parent and child tables;
+
+parent = Parent(id=1,name='aaaaa')
+child_1  = Child(id=1,name='child_1')
+child_2  = Child(id=2,name='child_2')
+
+parent.children = [child_1,child_2]
+
+session.add(parent)
+session.commit()
+session.close()
+'''
+
+# query tables;
+
+for each in session.query(Parent,Child).filter(Parent.id==1).filter(Child.id==2).all():
+    parent,child = each
+    print parent.id,parent.name
+    print child.id,child.name
+
+
+
+
+
+ 
